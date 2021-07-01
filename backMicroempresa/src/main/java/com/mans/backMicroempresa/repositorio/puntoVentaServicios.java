5c7d600776b4ef3b69bcbcbcfda2907800d2ab0d
drack25 <andynts25@gmail.com>
2021-07-01 18:16:51 -0500

    ms

---
+++ backMicroempresa/src/main/java/com/mans/backMicroempresa/repositorio/puntoVentaServicios.java
@@ -0,0 +1,12 @@
+package com.mans.backMicroempresa.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mans.backMicroempresa.entidades.PuntoVenta;
+
+@Repository
+
+public interface puntoVentaServicios extends JpaRepository <PuntoVenta, Long>{
+
+}

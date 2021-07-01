c93b320afbcdd84e6fdb7d517a9e3d962b892df8
RichardG20105 <richard20105@hotmail.com>
2021-06-30 19:01:56 -0500

    0.0

---
+++ backMicroempresa/src/main/java/com/mans/backMicroempresa/BackMicroempresaApplication.java
@@ -0,0 +1,13 @@
+package com.mans.backMicroempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackMicroempresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackMicroempresaApplication.class, args);
+	}
+
+}
---
+++ backMicroempresa/src/test/java/com/mans/backMicroempresa/BackMicroempresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mans.backMicroempresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackMicroempresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
